72b71a36a12478d46d369f706a261ca31414b159
kamenifrancis <blickson57@gmail.com>
2023-02-06 16:24:41 +0100

    checkpoint props

---
+++ src/profile/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HandleName from './Image'
+const Profile = () => {
+  return (
+    <div>
+      < HandleName/> 
+       name = {"francis"}, 
+       professsion = {'developpeur'},
+       age = {'24'},
+
+    </div>
+  )
+}
+
+export default Profile
